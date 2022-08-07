0c1483c481885687a6ef5b801e0d55ccc6a022a7
Hyun Woo <hwsa10041@gmail.com>
2022-08-07 09:55:45 +0900

    FYB Project Setting
    기초 세팅 완료

---
+++ src/main/java/school/bonobono/fyb/FybApplication.java
@@ -0,0 +1,13 @@
+package school.bonobono.fyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FybApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FybApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/bonobono/fyb/FybApplicationTests.java
@@ -0,0 +1,13 @@
+package school.bonobono.fyb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FybApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
