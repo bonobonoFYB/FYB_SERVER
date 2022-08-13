2da60fe07e595a4e0544d254ba155a9a1df776ef
Hyun Woo <hwsa10041@gmail.com>
2022-08-13 21:31:08 +0900

    Wishlist 기능 구현을 위한 추가 상태코드 구현

--- src/main/java/school/bonobono/fyb/Model/StatusTrue.java
+++ src/main/java/school/bonobono/fyb/Model/StatusTrue.java
@@ -12,7 +12,10 @@ public enum StatusTrue {
     LOGIN_STATUS_TRUE(200,"로그인 성공"),
     READ_STATUS_TRUE(200,"정보 불러오기 성공"),
     LOGOUT_STATUS_TRUE(200,"로그아웃 성공"),
-    UPDATE_STATUS_TURE(200, "회원정보 업데이트 성공");
+    UPDATE_STATUS_TURE(200, "회원정보 업데이트 성공"),
+    WISHLIST_ADD_STATUS_TRUE(200,"장바구니 등록 성공"),
+    WISHLIST_DELETE_STATUS_TRUE(200,"장바구니 삭제 성공"),
+    WISHLIST_UPDATE_STATUS_TRUE(200,"장바구니 업데이트 성공");
 
     private final Integer status;
     private final String statusMessage;
