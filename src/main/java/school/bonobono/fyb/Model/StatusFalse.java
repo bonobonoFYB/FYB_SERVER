1ca842b3f1205b08fbef9af941709b3e48fafa60
Hyun Woo <hwsa10041@gmail.com>
2022-08-18 17:08:31 +0900

    실패 코드 Stringa 으로 변경

--- src/main/java/school/bonobono/fyb/Model/StatusFalse.java
+++ src/main/java/school/bonobono/fyb/Model/StatusFalse.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 @Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StatusFalse {
-    JWT_CREDENTIALS_STATUS_FALSE(401,"로그인이 유효하지 않습니다."),
-    PASSWORD_CHANGE_STATUS_FALSE(403, "현재 비밀번호가 일치하지 않습니다." ),
-    USER_DELETE_STATUS_FALSE(403, "비밀번호가 일치하지 않아 탈퇴에 실패했습니다." );
+    JWT_CREDENTIALS_STATUS_FALSE("JWT_CREDENTIALS_STATUS_FALSE","로그인이 유효하지 않습니다."),
+    PASSWORD_CHANGE_STATUS_FALSE("PASSWORD_CHANGE_STATUS_FALSE", "현재 비밀번호가 일치하지 않습니다." ),
+    USER_DELETE_STATUS_FALSE("USER_DELETE_STATUS_FALSE", "비밀번호가 일치하지 않아 탈퇴에 실패했습니다." );
 
 
-    private final Integer status;
+    private final String status;
     private final String statusMessage;
 }
