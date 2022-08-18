67c9a634deb7ff659c8da2e3068d9a1490c79727
Hyun Woo <hwsa10041@gmail.com>
2022-08-19 00:09:09 +0900

    익셉션 상태코드 추가
    장바구니 비어있을 경우 핸들링

--- src/main/java/school/bonobono/fyb/Exception/CustomErrorCode.java
+++ src/main/java/school/bonobono/fyb/Exception/CustomErrorCode.java
@@ -8,13 +8,16 @@ import lombok.Getter;
 public enum CustomErrorCode {
     SEARCH_EMPTY("검색 항목이 존재하지 않습니다."),
     JWT_CREDENTIALS_STATUS_FALSE("로그인이 유효하지 않습니다."),
+    JWT_TOKEN_IS_NULL("토큰에 값이 존재하지 않습니다."),
     PASSWORD_CHANGE_STATUS_FALSE( "현재 비밀번호가 일치하지 않습니다." ),
     USER_DELETE_STATUS_FALSE( "비밀번호가 일치하지 않아 탈퇴에 실패했습니다." ),
     LOGIN_FALSE("아이디 또는 비밀번호를 잘못 입력하였습니다."),
     NOT_FOUND_USER("해당 이메일의 유저가 존재하지 않습니다"),
     DUPLICATE_USER("해당 이메일의 가입자가 이미 존재합니다."),
+    WISHLIST_EMPTY("장바구니가 비어있습니다."),
     // 알수 없는 오류의 처리
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
+    AUTH_CHECK("권한 오류입니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
 
     private final String statusMessage;
