874d88be00a0c7c3cbf6cb6687094ba9eb10e9fb
Hyun Woo <hwsa10041@gmail.com>
2022-08-18 15:28:18 +0900

    회원 탈퇴를 위한 상태 코드 구현

--- src/main/java/school/bonobono/fyb/Model/StatusFalse.java
+++ src/main/java/school/bonobono/fyb/Model/StatusFalse.java
@@ -9,7 +9,9 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StatusFalse {
     JWT_CREDENTIALS_STATUS_FALSE(401,"로그인이 유효하지 않습니다."),
-    PASSWORD_CHANGE_STATUS_FALSE(403, "현재 비밀번호가 일치하지 않습니다." );
+    PASSWORD_CHANGE_STATUS_FALSE(403, "현재 비밀번호가 일치하지 않습니다." ),
+    USER_DELETE_STATUS_FALSE(403, "비밀번호가 일치하지 않아 탈퇴에 실패했습니다." );
+
 
     private final Integer status;
     private final String statusMessage;
--- src/main/java/school/bonobono/fyb/Model/StatusTrue.java
+++ src/main/java/school/bonobono/fyb/Model/StatusTrue.java
@@ -8,16 +8,17 @@ import lombok.Getter;
 @Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StatusTrue {
-    REGISTER_STATUS_TRUE(200,"회원가입 성공"),
-    LOGIN_STATUS_TRUE(200,"로그인 성공"),
-    READ_STATUS_TRUE(200,"정보 불러오기 성공"),
-    LOGOUT_STATUS_TRUE(200,"로그아웃 성공"),
+    REGISTER_STATUS_TRUE(200, "회원가입 성공"),
+    LOGIN_STATUS_TRUE(200, "로그인 성공"),
+    READ_STATUS_TRUE(200, "정보 불러오기 성공"),
+    LOGOUT_STATUS_TRUE(200, "로그아웃 성공"),
     UPDATE_STATUS_TURE(200, "회원정보 업데이트 성공"),
-    WISHLIST_ADD_STATUS_TRUE(200,"장바구니 등록 성공"),
-    WISHLIST_DELETE_STATUS_TRUE(200,"장바구니 삭제 성공"),
-    WISHLIST_UPDATE_STATUS_TRUE(200,"장바구니 업데이트 성공"),
-    PHONE_CHECK_STATUS_TRUE(200,"휴대폰 인증번호 보내기 성공"),
-    PASSWORD_CHANGE_STATUS_TRUE(200,"비밀번호 변경 성공" );
+    WISHLIST_ADD_STATUS_TRUE(200, "장바구니 등록 성공"),
+    WISHLIST_DELETE_STATUS_TRUE(200, "장바구니 삭제 성공"),
+    WISHLIST_UPDATE_STATUS_TRUE(200, "장바구니 업데이트 성공"),
+    PHONE_CHECK_STATUS_TRUE(200, "휴대폰 인증번호 보내기 성공"),
+    PASSWORD_CHANGE_STATUS_TRUE(200, "비밀번호 변경 성공"),
+    USER_DELETE_STATUS_TRUE(200, "회원탈퇴 성공");
 
     private final Integer status;
     private final String statusMessage;
