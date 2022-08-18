9827d4a347296b4191823c79e071dfb0f2244038
Hyun Woo <hwsa10041@gmail.com>
2022-08-18 17:43:29 +0900

    로그인 실패시 상태코드 구현

--- src/main/java/school/bonobono/fyb/Exception/CustomErrorCode.java
+++ src/main/java/school/bonobono/fyb/Exception/CustomErrorCode.java
@@ -10,6 +10,7 @@ public enum CustomErrorCode {
     JWT_CREDENTIALS_STATUS_FALSE("로그인이 유효하지 않습니다."),
     PASSWORD_CHANGE_STATUS_FALSE( "현재 비밀번호가 일치하지 않습니다." ),
     USER_DELETE_STATUS_FALSE( "비밀번호가 일치하지 않아 탈퇴에 실패했습니다." ),
+    LOGIN_FALSE("아이디 또는 비밀번호를 잘못 입력하였습니다."),
     // 알수 없는 오류의 처리
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다.");
