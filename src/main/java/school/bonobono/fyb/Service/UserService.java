5c2dcb6e9716a4b27bff10d833e61c364533078f
Hyun Woo <hwsa10041@gmail.com>
2022-08-18 14:50:46 +0900

    휴대폰 인증 및 비밀번호 변경 구현 Service

--- src/main/java/school/bonobono/fyb/Service/UserService.java
+++ src/main/java/school/bonobono/fyb/Service/UserService.java
@@ -3,14 +3,14 @@ package school.bonobono.fyb.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import school.bonobono.fyb.Dto.TokenInfoResponseDto;
-import school.bonobono.fyb.Dto.UserReadDto;
-import school.bonobono.fyb.Dto.UserRegisterDto;
-import school.bonobono.fyb.Dto.UserUpdateDto;
+import school.bonobono.fyb.Dto.*;
 import school.bonobono.fyb.Entity.Authority;
 import school.bonobono.fyb.Entity.FybUser;
 import school.bonobono.fyb.Exception.DuplicateMemberException;
@@ -23,8 +23,7 @@ import school.bonobono.fyb.Util.SecurityUtil;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.constant.Constable;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Objects;
+import java.util.*;
 
 import static school.bonobono.fyb.Model.Model.AUTHORIZATION_HEADER;
 
@@ -94,27 +93,22 @@ public class UserService {
         if (!tokenCredEntialsValidate(headerRequest))
             return StatusFalse.JWT_CREDENTIALS_STATUS_FALSE;
 
-        Long userid = getTokenInfo().getId();
-        String userpw = getTokenInfo().getPw();
-        String useremail = getTokenInfo().getEmail();
-        LocalDateTime localDateTime = getTokenInfo().getCreateAt();
-
         Authority authority = Authority.builder()
                 .authorityName("ROLE_USER")
                 .build();
 
         userRepository.save(
                 FybUser.builder()
-                        .id(userid)
-                        .email(useremail)
-                        .pw(userpw)
+                        .id(getTokenInfo().getId())
+                        .email(getTokenInfo().getEmail())
+                        .pw(getTokenInfo().getPw())
                         .name(request.getName())
                         .authorities(Collections.singleton(authority))
                         .gender(request.getGender())
                         .height(request.getHeight())
                         .weight(request.getWeight())
                         .age(request.getAge())
-                        .createAt(localDateTime)
+                        .createAt(getTokenInfo().getCreateAt())
                         .build()
         );
 
@@ -149,5 +143,68 @@ public class UserService {
             return false;
         }
         return true;
+    }
+
+    public Map<Object, Object> certifiedPhoneNumber(PhoneCheckDto.Request request, String randNum) {
+        String api_key = "NCSBDTMXRMDGUIFD";
+        String api_secret = "S917YGKP2H2IFYE0P9ONJBTFA2EDCV3J";
+        Message coolsms = new Message(api_key, api_secret);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("to", request.getPnum());
+        params.put("from", "010-4345-4377");
+        params.put("type", "SMS");
+        params.put("text", "FYB 휴대폰인증 : 인증번호는" + "[" + randNum + "]" + "입니다.");
+        params.put("app_version", "test app 1.2");
+
+        try {
+            JSONObject obj = (JSONObject) coolsms.send(params);
+            System.out.println(obj.toString());
+        } catch (CoolsmsException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+        }
+
+        Map<Object, Object> send = new HashMap<>();
+        send.put("randNum", randNum);
+        return send;
+    }
+
+    public Constable PwChangeUser(PwChangeDto.Request request) {
+
+        if (userRepository.findByEmail(request.getEmail()).orElse(null) == null) {
+            throw new RuntimeException("해당 이메일을 가진 유저가 없습니다.");
+        }
+
+        if (passwordEncoder.matches(request.getPw(), getTokenInfo().getPw())) // 입력한 비밀번호와 현재 비밀번호가 맞을 경우
+        {
+            Authority authority = Authority.builder()
+                    .authorityName("ROLE_USER")
+                    .build();
+
+            userRepository.save(
+                    FybUser.builder()
+                            .id(getTokenInfo().getId())
+                            .email(getTokenInfo().getEmail())
+                            .pw(passwordEncoder.encode(request.getNewPw()))
+                            .name(getTokenInfo().getName())
+                            .authorities(Collections.singleton(authority))
+                            .gender(getTokenInfo().getGender())
+                            .height(getTokenInfo().getHeight())
+                            .weight(getTokenInfo().getWeight())
+                            .age(getTokenInfo().getAge())
+                            .createAt(getTokenInfo().getCreateAt())
+                            .authorities(Collections.singleton(authority))
+                            .build()
+            );
+
+            return StatusTrue.PASSWORD_CHANGE_STATUS_TRUE;
+        } else {
+            return StatusFalse.PASSWORD_CHANGE_STATUS_FALSE;
+        }
+    }
+
+    public Constable PwLostChange(PwChangeDto.Request request) {
+        
     }
 }
