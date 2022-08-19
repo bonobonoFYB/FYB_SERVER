f5da9219c67475f98f1aff51bd4f21fe6e003263
Hyun Woo <hwsa10041@gmail.com>
2022-08-19 14:00:01 +0900

    API키 상수화

--- src/main/java/school/bonobono/fyb/Model/Model.java
+++ src/main/java/school/bonobono/fyb/Model/Model.java
@@ -3,4 +3,7 @@ package school.bonobono.fyb.Model;
 public class Model {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String CHECK_API_KEY = "NCSBDTMXRMDGUIFD";
+    public static final String CHEKC_API_SECRET = "S917YGKP2H2IFYE0P9ONJBTFA2EDCV3J";
+
 }
