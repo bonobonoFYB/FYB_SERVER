f40c7bda04a1ba1d1bb9465b0e6f561e34422cae
Hyun Woo <hwsa10041@gmail.com>
2022-08-29 20:25:23 +0900

    옷장 추가하기 구현 Controller

--- src/main/java/school/bonobono/fyb/Controller/MyClosetController.java
+++ src/main/java/school/bonobono/fyb/Controller/MyClosetController.java
@@ -2,13 +2,12 @@ package school.bonobono.fyb.Controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.bonobono.fyb.Dto.MyClosetDto;
 import school.bonobono.fyb.Service.MyClosetService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.constant.Constable;
 import java.util.List;
 
 @RestController
@@ -18,8 +17,25 @@ import java.util.List;
 public class MyClosetController {
     private final MyClosetService myClosetService;
 
+    // 옷장 조회
     @GetMapping("read")
     public List<MyClosetDto.readResponse> readMyCloset(HttpServletRequest headerRequest){
         return myClosetService.readMyCloset(headerRequest);
+    }
+
+    // 옷장 추가하기
+    @PostMapping("add")
+    public Constable addMyCloset(
+            @RequestBody final MyClosetDto.addRequest request, HttpServletRequest headerRequest
+    ){
+        return myClosetService.addMyCloset(request,headerRequest);
+    }
+
+    // 옷장 삭제
+    @PostMapping("delete")
+    public Constable deleteMyCloset(
+            @RequestParam(name = "id") final Long id, HttpServletRequest headerRequest
+    ){
+        return myClosetService.deleteCloset(id,headerRequest);
     }
 }
