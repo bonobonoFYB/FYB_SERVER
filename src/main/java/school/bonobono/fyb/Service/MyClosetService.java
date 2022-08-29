f792de092423323a83d5b8de86cc5cdc6782d538
Hyun Woo <hwsa10041@gmail.com>
2022-08-29 20:25:35 +0900

    옷장 추가하기 구현 Service

--- src/main/java/school/bonobono/fyb/Service/MyClosetService.java
+++ src/main/java/school/bonobono/fyb/Service/MyClosetService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school.bonobono.fyb.Dto.MyClosetDto;
 import school.bonobono.fyb.Dto.TokenInfoResponseDto;
+import school.bonobono.fyb.Entity.MyCloset;
 import school.bonobono.fyb.Exception.CustomException;
 import school.bonobono.fyb.Repository.MyClosetRepository;
 import school.bonobono.fyb.Repository.TokenRepository;
@@ -12,12 +13,14 @@ import school.bonobono.fyb.Repository.UserRepository;
 import school.bonobono.fyb.Util.SecurityUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.constant.Constable;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static school.bonobono.fyb.Exception.CustomErrorCode.JWT_CREDENTIALS_STATUS_FALSE;
+import static school.bonobono.fyb.Exception.CustomErrorCode.MY_CLOSET_EMPTY;
 import static school.bonobono.fyb.Model.Model.AUTHORIZATION_HEADER;
+import static school.bonobono.fyb.Model.StatusTrue.MY_CLOSET_ADD_STATUS_TRUE;
 
 @Service
 @RequiredArgsConstructor
@@ -27,17 +30,13 @@ public class MyClosetService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
 
-    public List<MyClosetDto.readResponse> readMyCloset(HttpServletRequest headerRequest) {
-        tokenCredEntialsValidate(headerRequest);
-
-        List<MyClosetDto.readResponse> list = myClosetRepository
-                .findByUid(getTokenInfo().getId())
-                .stream()
-                .map(MyClosetDto.readResponse::Response).toList();
-        return list;
-    }
-
     // Validation 및 단순화
+
+    private static void readMyClosetValidate(List<MyClosetDto.readResponse> list) {
+        if (list.isEmpty()) {
+            throw new CustomException(MY_CLOSET_EMPTY);
+        }
+    }
 
     private TokenInfoResponseDto getTokenInfo() {
         return TokenInfoResponseDto.Response(
@@ -54,5 +53,38 @@ public class MyClosetService {
                 .orElseThrow(
                         () -> new CustomException(JWT_CREDENTIALS_STATUS_FALSE)
                 );
+    }
+
+    // Service
+
+    public List<MyClosetDto.readResponse> readMyCloset(HttpServletRequest headerRequest) {
+        tokenCredEntialsValidate(headerRequest);
+
+        List<MyClosetDto.readResponse> list = myClosetRepository
+                .findByUid(getTokenInfo().getId())
+                .stream()
+                .map(MyClosetDto.readResponse::Response).toList();
+
+        readMyClosetValidate(list);
+
+        return list;
+    }
+
+    public Constable addMyCloset(MyClosetDto.addRequest request,HttpServletRequest headerRequest) {
+        tokenCredEntialsValidate(headerRequest);
+
+        myClosetRepository.save(
+                MyCloset.builder()
+                        .uid(getTokenInfo().getId())
+                        .pkind(request.getPkind())
+                        .pname(request.getPname())
+                        .pnotes(request.getPnotes())
+                        .build()
+        );
+        return MY_CLOSET_ADD_STATUS_TRUE;
+    }
+
+    public Constable deleteCloset(Long id, HttpServletRequest headerRequest) {
+        
     }
 }
