f825101c701f62b553c516d43a858fb4860576e7
Hyun Woo <hwsa10041@gmail.com>
2022-09-21 15:42:25 +0900

    쇼핑몰 사용자 데이터 분석을 위한 Dto 구현

---
+++ src/main/java/school/bonobono/fyb/Dto/ShopDataDto.java
@@ -0,0 +1,24 @@
+package school.bonobono.fyb.Dto;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+public class ShopDataDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request{
+        private Long sid;
+        private Character gender;
+        private Integer age;
+        private String surl;
+        // 미구현
+        private Integer height;
+        private Integer weight;
+    }
+}
