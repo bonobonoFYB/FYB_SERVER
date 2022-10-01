bceef72999169a5694d947e5cc64ded260b4b8b7
Hyun Woo <hwsa10041@gmail.com>
2022-10-01 12:19:47 +0900

    사용자 최다 조회수 Controller 구현

--- src/main/java/school/bonobono/fyb/Controller/ShopController.java
+++ src/main/java/school/bonobono/fyb/Controller/ShopController.java
@@ -2,11 +2,14 @@ package school.bonobono.fyb.Controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import school.bonobono.fyb.Dto.ShopDataDto;
 import school.bonobono.fyb.Dto.ShopDto;
 import school.bonobono.fyb.Entity.Shop;
+import school.bonobono.fyb.Entity.ShopData;
 import school.bonobono.fyb.Service.ShopService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,10 +31,17 @@ public class ShopController {
         return shopService.getAllShopAndUserInfo(request);
     }
 
+    // 사용자 최다조회수 API
+    @GetMapping("rank/all")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<List<ShopDto.Response>> getMostViewed(HttpServletRequest request) {
+        return shopService.getMostViewed(request);
+    }
+
     // 쇼핑몰 클릭시 쇼핑몰 이용자의 빅데이터 분석
     @PostMapping
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public HashMap<Object, Object> saveShopData(
+    public ResponseEntity<HashMap<Object, Object>> saveShopData(
             @RequestBody final ShopDataDto.Request request, HttpServletRequest headerRequest
     ) {
         return shopService.saveShopData(request, headerRequest);
