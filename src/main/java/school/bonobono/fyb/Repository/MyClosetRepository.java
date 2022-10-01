c4595c154e6d81da75a074ee8e6b45eafac4643f
Hyun Woo <hwsa10041@gmail.com>
2022-10-01 16:37:19 +0900

    잘못 선언된 JPA 구문 수정

--- src/main/java/school/bonobono/fyb/Repository/MyClosetRepository.java
+++ src/main/java/school/bonobono/fyb/Repository/MyClosetRepository.java
@@ -4,11 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.bonobono.fyb.Entity.MyCloset;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MyClosetRepository extends JpaRepository<MyCloset, Long> {
-    Optional<MyCloset> findByUid(Long uid);
-
+    List<MyCloset> findByUid(Long uid);
     Optional<MyCloset> findByPname(String pname);
 }
