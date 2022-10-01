e70a93f4e34773234338f01ba1ca8b11c9415c92
Hyun Woo <hwsa10041@gmail.com>
2022-10-01 16:37:36 +0900

    내 옷장 이미지 추가 기능 구현 Controller

--- src/main/java/school/bonobono/fyb/Controller/MyClosetController.java
+++ src/main/java/school/bonobono/fyb/Controller/MyClosetController.java
@@ -2,6 +2,8 @@ package school.bonobono.fyb.Controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import school.bonobono.fyb.Dto.MyClosetDto;
@@ -20,20 +22,23 @@ public class MyClosetController {
 
     // 옷장 조회
     @GetMapping("closet")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public List<MyClosetDto.readResponse> readMyCloset(HttpServletRequest headerRequest){
         return myClosetService.readMyCloset(headerRequest);
     }
 
-/*    // 옷 사진 추가 등록
+    // 옷 사진 추가 등록
     @PutMapping("closet")
-    public Constable updateImage(
-            @RequestParam("file") MultipartFile multipartFile
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<Object> updateImage(
+            @RequestParam("file") MultipartFile multipartFile, @RequestParam("id") Long id
     ) {
-        return myClosetService.updateImage(multipartFile);
-    }*/
+        return myClosetService.updateImage(multipartFile,id);
+    }
 
     // 옷장 추가하기
     @PostMapping("closet")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public List<Object> addMyCloset(
             @RequestBody final MyClosetDto.addRequest request, HttpServletRequest headerRequest
     ){
@@ -42,6 +47,7 @@ public class MyClosetController {
 
     // 옷장 삭제
     @DeleteMapping("closet")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public Constable deleteMyCloset(
             @RequestBody final MyClosetDto.deleteRequest request, HttpServletRequest headerRequest
     ){
@@ -50,6 +56,7 @@ public class MyClosetController {
 
     // 옷장 업데이트
     @PatchMapping("closet")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public Constable deleteMyCloset(
             @RequestBody final MyClosetDto.readResponse request, HttpServletRequest headerRequest
     ){
