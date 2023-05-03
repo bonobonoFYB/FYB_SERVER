23f72a3c3417c090b7c2000768398845a54586fd
Hyun Woo <hwsa10041@gmail.com>
2023-05-03 19:25:13 +0900

    [REFACTOR] 불필요한 Entity 제거

--- src/main/java/school/bonobono/fyb/domain/shop/Repository/ShopDataRepository.java
+++
@@ -1,9 +0,0 @@
-package school.bonobono.fyb.domain.shop.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import school.bonobono.fyb.domain.shop.Entity.ShopData;
-
-@Repository
-public interface ShopDataRepository extends JpaRepository<ShopData, Long> {
-}
